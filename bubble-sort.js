9160f627491d06c664e93ca9afb964737f08ba4e
saqee <saqeeb108@gmail.com>
2023-01-25 15:48:47 +0100

    insertion-sort

---
+++ bubble-sort.js
@@ -0,0 +1,15 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      }
+    }
+  }
+  return array
+}
+
+let a = bubbleSort([1, 4, 2, 5, -2, 3])
+console.log(a)
